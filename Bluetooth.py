e89f1732ea27a885040b84e17f22acbfa4f959d9
ASAPH BONIFACIO GUIMARAES ARRUDA <20241014040010@ifrn.local>
2024-10-29 10:21:20 -0300

    ADD: Teste com Bluethoof

---
+++ Bluetooth.py
@@ -0,0 +1,6 @@
+import bluetooth
+nearby_devices = bluetooth.discover_devices()
+print(nearby_devices)
+print("Encontrados: ",len(nearby_devices),"dispositivos: ")
+for bdaddr in nearby_devices:
+    print("\t",nearby_devices.index(bdaddr),bluetooth.lookup_name( bdaddr ))
